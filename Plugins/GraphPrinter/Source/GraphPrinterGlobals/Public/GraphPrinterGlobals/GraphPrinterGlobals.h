8befd7e18840b760bd584cd4baabaca2a0721b7d
Naotsun <51815450+Naotsun19B@users.noreply.github.com>
2023-06-23 23:23:55 +0900

    Implemented redefinition countermeasures for the version macro

--- Plugins/GraphPrinter/Source/GraphPrinterGlobals/Public/GraphPrinterGlobals/GraphPrinterGlobals.h
+++ Plugins/GraphPrinter/Source/GraphPrinterGlobals/Public/GraphPrinterGlobals/GraphPrinterGlobals.h
@@ -17,37 +17,59 @@ namespace GraphPrinter
 /**
  * Macros to support each engine version.
  */
+#ifndef ENGINE_VERSION_NUMBER
 #define ENGINE_VERSION_NUMBER(MajorVersion, MinorVersion) (MajorVersion * 100 + MinorVersion)
+#endif
+#ifndef COMPARE_ENGINE_VERSION
 #define COMPARE_ENGINE_VERSION(MajorVersion, MinorVersion) ENGINE_VERSION_NUMBER(ENGINE_MAJOR_VERSION, ENGINE_MINOR_VERSION) >= ENGINE_VERSION_NUMBER(MajorVersion, MinorVersion)
+#endif
 
+#ifndef UE_5_02_OR_LATER
+#if COMPARE_ENGINE_VERSION(5, 2)
+#define UE_5_02_OR_LATER 1
+#else
+#define UE_5_02_OR_LATER 0
+#endif
+#endif
+
+#ifndef UE_5_01_OR_LATER
 #if COMPARE_ENGINE_VERSION(5, 1)
 #define UE_5_01_OR_LATER 1
 #else
 #define UE_5_01_OR_LATER 0
 #endif
+#endif
 
+#ifndef UE_5_00_OR_LATER
 #if COMPARE_ENGINE_VERSION(5, 0)
 #define UE_5_00_OR_LATER 1
 #else
 #define UE_5_00_OR_LATER 0
 #endif
+#endif
 
+#ifndef UE_4_25_OR_LATER
 #if COMPARE_ENGINE_VERSION(4, 25)
 #define UE_4_25_OR_LATER 1
 #else
 #define UE_4_25_OR_LATER 0
 #endif
+#endif
 
+#ifndef UE_4_24_OR_LATER
 #if COMPARE_ENGINE_VERSION(4, 24)
 #define UE_4_24_OR_LATER 1
 #else
 #define UE_4_24_OR_LATER 0
 #endif
+#endif
 
+#ifndef UE_4_22_OR_LATER
 #if COMPARE_ENGINE_VERSION(4, 22)
 #define UE_4_22_OR_LATER 1
 #else
 #define UE_4_22_OR_LATER 0
+#endif
 #endif
 
 /**
